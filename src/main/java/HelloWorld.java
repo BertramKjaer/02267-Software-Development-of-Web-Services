b145404b3081f4065e4ec9b0664a6aa59de84df0
BertramKjaer <kjaerbertram@gmail.com>
2026-01-07 12:39:50 +0100

    Made simple hello world program + test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static String getMessage() {
+        return "Hello, World!";
+    }
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void testGetMessage() {
+        assertEquals("Hello, World!", HelloWorld.getMessage());
+    }
+}
